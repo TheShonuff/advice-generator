b62c9aad6eefdcaecee9170545565114417de2ef
TheShonuff <jirvine124@gmail.com>
2022-03-11 00:40:34 -0700

    fetch loads new quote with onclick event

--- somejava.js
+++ somejava.js
@@ -1,6 +1,9 @@
+const adviceID = document.querySelector('.adviceID');
+const advice = document.querySelector('.advice');
+
 
 async function fetchAdvice() {
-    let response = await fetch('https://api.adviceslip.com/advice');
+    let response = await fetch('https://api.adviceslip.com/advice', {cache: 'no-store'});
 
     console.log(response.status);
     console.log(response.statusText);
@@ -9,26 +12,30 @@ async function fetchAdvice() {
         let data = await response.json();
         console.log(data.slip.advice);
         console.log(data.slip.id);
-        return data;
+
+        adviceID.innerText = `ADVICE #` + data['slip']['id'];
+        advice.innerText = `"` + data['slip']['advice'] +`"`;
+
     }
 }
 
-async function renderAdvice() {
-    let advice = await fetchAdvice();
+
+
+
+// async function renderAdvice() {
+//     let advice = await fetchAdvice();
     
-    let advicehtml = '';
-    let adviceIDhtml = '';
-    let advicesegment =   `"${advice.slip.advice}."` ;
-    let adviceIDsegment = `${advice.slip.id}`;
+//     let advicehtml = '';
+//     let adviceIDhtml = '';
+//     let advicesegment =   `"${advice.slip.advice}."` ;
+//     let adviceIDsegment = `${advice.slip.id}`;
 
-    advicehtml += advicesegment;
-    adviceIDhtml += adviceIDsegment; 
+//     advicehtml += advicesegment;
+//     adviceIDhtml += adviceIDsegment; 
 
-    let container1 = document.querySelector('.advice');
-    container1.innerHTML = advicehtml;
+//     let container1 = document.querySelector('.advice');
+//     container1.innerHTML = advicehtml;
 
-    let container2 = document.querySelector('.idnum');
-    container2.innerHTML = adviceIDhtml;
-}
-
-
+//     let container2 = document.querySelector('.idnum');
+//     container2.innerHTML = adviceIDhtml;
+// }
