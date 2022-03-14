2f70b7b15c07b5f8175a3e9332933dbbdf6f0335
TheShonuff <jirvine124@gmail.com>
2022-03-13 21:17:56 -0700

    fix html issues

--- somejava.js
+++ somejava.js
@@ -1,24 +1,21 @@
-const adviceID = document.querySelector('.adviceID');
-const advice = document.querySelector('.advice');
-
+const adviceID = document.querySelector(".adviceID");
+const advice = document.querySelector(".advice");
 
 async function fetchAdvice() {
-    let response = await fetch('https://api.adviceslip.com/advice', {cache: 'no-store'});
+  let response = await fetch("https://api.adviceslip.com/advice", {
+    cache: "no-store",
+  });
 
-    console.log(response.status);
-    console.log(response.statusText);
+  console.log(response.status);
+  console.log(response.statusText);
 
-    if (response.status === 200) {
-        let data = await response.json();
-        console.log(data.slip.advice);
-        console.log(data.slip.id);
+  if (response.status === 200) {
+    let data = await response.json();
+    console.log(data.slip.advice);
+    console.log(data.slip.id);
 
+    adviceID.innerHTML = `<h1>ADVICE #${data.slip.id}<h1>`;
 
-        adviceID.innerHTML = `<p>ADVICE #${data.slip.id}</p>`
-
-        advice.innerHTML = `<blockquote>"${data.slip.advice}"</blockquote>`
-    }
+    advice.innerHTML = `<blockquote>"${data.slip.advice}"</blockquote>`;
+  }
 }
-
-
-
